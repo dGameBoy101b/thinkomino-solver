88ba12e324b2290d063e2a467483ff0922edba66
dGameBoy101b <44672112+dGameBoy101b@users.noreply.github.com>
2023-01-01 13:48:10 +0800

    Expanded item extractor unit tests

--- tests/item_extractor_test.py
+++ tests/item_extractor_test.py
@@ -3,6 +3,9 @@ from item_extractor import extract_item
 
 class ConnectedListMapperTest(TestCase):
 
+	def test_noop(self):
+		self.assertEqual(extract_item([1,2,3], []), [1,2,3])
+
 	def test_flat_index(self):
 		self.assertEqual(extract_item([1,2,3], [0]), 1)
 
@@ -18,3 +21,21 @@ class ConnectedListMapperTest(TestCase):
 	def test_nested_mixed(self):
 		self.assertEqual(extract_item([{'a':1,'b':2,'c':3},{'d':4,'e':5,'f':6},{'g':7,'h':8,'i':9}], [0,'b']), 2)
 		self.assertEqual(extract_item({'a':[1,2,3],'b':[4,5,6],'c':[7,8,9]}, ['a',1]), 2)
+
+	def test_index_error(self):
+		with self.assertRaises(IndexError):
+			extract_item([1,2,3], [3])
+		with self.assertRaises(IndexError):
+			extract_item({'a':[1,2,3],'b':[4,5,6],'c':[7,8,9]}, ['c',4])
+
+	def test_type_error(self):
+		with self.assertRaises(TypeError):
+			extract_item(123, [0])
+		with self.assertRaises(TypeError):
+			extract_item([1,2,3], [0,0,0])
+
+	def test_key_error(self):
+		with self.assertRaises(KeyError):
+			extract_item({'a':1,'b':2,'c':3}, ['abc'])
+		with self.assertRaises(KeyError):
+			extract_item([{'a':1,'b':2,'c':3},{'d':4,'e':5,'f':6},{'g':7,'h':8,'i':9}], [1,'abd'])
