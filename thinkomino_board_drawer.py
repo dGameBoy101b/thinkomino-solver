02a0c37927ea4870452894c8f6c57fb2cc8c64de
dGameBoy101b <44672112+dGameBoy101b@users.noreply.github.com>
2023-01-09 17:05:23 +0800

    Added line settings to thinkomino tile drawer.

--- thinkomino_board_drawer.py
+++ thinkomino_board_drawer.py
@@ -2,7 +2,7 @@ from turtle import RawTurtle
 from thinkomino_board import ThinkominoBoard
 from thinkomino_tile_drawer import draw_tile
 
-def draw_board(board: ThinkominoBoard, turtle: RawTurtle, radius: float, red = 'red', orange = 'orange', yellow = 'yellow', green = 'green', blue = 'blue', purple = 'purple'):
+def draw_board(board: ThinkominoBoard, turtle: RawTurtle, radius: float, red = 'red', orange = 'orange', yellow = 'yellow', green = 'green', blue = 'blue', purple = 'purple', inner_colour = 'black', inner_width: int = 1, outer_colour = 'grey', outer_width: int = 5):
 	TILE_HEADING = turtle.heading()
 	turtle.setheading(turtle.heading() + 90)
 	coefficent = .5
@@ -11,7 +11,7 @@ def draw_board(board: ThinkominoBoard, turtle: RawTurtle, radius: float, red = '
 		heading = turtle.heading()
 		turtle.forward((1.5 + coefficent) * radius)
 		turtle.setheading(TILE_HEADING)
-		draw_tile(tile, turtle, radius, red, orange, yellow, green, blue, purple)
+		draw_tile(tile, turtle, radius, red, orange, yellow, green, blue, purple, inner_colour, inner_width, outer_colour, outer_width)
 		turtle.penup()
 		turtle.setheading(heading)
 		turtle.backward((1.5 + coefficent) * radius)
@@ -29,3 +29,4 @@ if __name__ == '__main__':
 	TILE = ThinkominoTile(ThinkominoColour.RED, ThinkominoColour.ORANGE, ThinkominoColour.YELLOW, ThinkominoColour.GREEN, ThinkominoColour.BLUE, ThinkominoColour.PURPLE)
 	BOARD = ThinkominoBoard(TILE,TILE,TILE,TILE,TILE,TILE)
 	draw_board(BOARD, turtle, 100)
+	pass
--- thinkomino_tile_drawer.py
+++ thinkomino_tile_drawer.py
@@ -2,7 +2,7 @@ from turtle import RawTurtle
 from thinkomino_tile import ThinkominoTile
 from thinkomino_colour import ThinkominoColour
 
-def draw_tile(tile: ThinkominoTile, turtle: RawTurtle, radius: float, red = 'red', orange = 'orange', yellow = 'yellow', green = 'green', blue = 'blue', purple = 'purple'):
+def draw_tile(tile: ThinkominoTile, turtle: RawTurtle, radius: float, red = 'red', orange = 'orange', yellow = 'yellow', green = 'green', blue = 'blue', purple = 'purple', inner_colour = 'black', inner_width: int = 1, outer_colour = 'grey', outer_width: int = 5):
 	COLOUR_MAP = {
 		ThinkominoColour.RED: red,
 		ThinkominoColour.ORANGE: orange,
@@ -15,10 +15,16 @@ def draw_tile(tile: ThinkominoTile, turtle: RawTurtle, radius: float, red = 'red
 	for colour in tile:
 		turtle.fillcolor(COLOUR_MAP[colour])
 		turtle.begin_fill()
+		turtle.pencolor(inner_colour)
+		turtle.pensize(inner_width)
 		turtle.forward(radius)
 		turtle.setheading(turtle.heading() + 120)
+		turtle.pencolor(outer_colour)
+		turtle.pensize(outer_width)
 		turtle.forward(radius)
 		turtle.setheading(turtle.heading() + 120)
+		turtle.pencolor(inner_colour)
+		turtle.pensize(inner_width)
 		turtle.forward(radius)
 		turtle.end_fill()
 		turtle.setheading(turtle.heading() + 180)
@@ -31,3 +37,4 @@ if __name__ == '__main__':
 	turtle.speed(0)
 	TILE = ThinkominoTile(ThinkominoColour.RED, ThinkominoColour.ORANGE, ThinkominoColour.YELLOW, ThinkominoColour.GREEN, ThinkominoColour.BLUE, ThinkominoColour.PURPLE)
 	draw_tile(TILE, turtle, 100)
+	pass
