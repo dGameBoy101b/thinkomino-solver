e60120862bc4ef6b90b2d71e993e3d0c1d5e8d0f
dGameBoy101b <rhysmader@gmail.com>
2025-06-22 21:00:58 +0800

    Added length assertions to permutation tests

--- tests/list_permuter_test.py
+++ tests/list_permuter_test.py
@@ -5,18 +5,21 @@ class ListPermuterTest(TestCase):
 
 	def test_empty(self):
 		permuter = permute_list([])
+		self.assertEqual(len(permuter), 1)
 		self.assertSequenceEqual(next(permuter), [])
 		with self.assertRaises(StopIteration):
 			next(permuter)
 
 	def test_one(self):
 		permuter = permute_list([1])
+		self.assertEqual(len(permuter), 1)
 		self.assertSequenceEqual(next(permuter), [1])
 		with self.assertRaises(StopIteration):
 			next(permuter)
 
 	def test_two(self):
 		permuter = permute_list(['a',3.4])
+		self.assertEqual(len(permuter), 2)
 		self.assertSequenceEqual(next(permuter), ['a',3.4])
 		self.assertSequenceEqual(next(permuter), [3.4, 'a'])
 		with self.assertRaises(StopIteration):
@@ -24,11 +27,13 @@ class ListPermuterTest(TestCase):
 
 	def test_three(self):
 		permuter = permute_list([['a',1], 3.4, 'string'])
+		self.assertEqual(len(permuter), 6)
 		self.assertSequenceEqual(next(permuter), [['a',1],3.4,'string'])
-		results = [x for x in permuter]
+		results = tuple(permuter)
+		self.assertTupleEqual
 		self.assertCountEqual(results, (
-			[['a',1],'string',3.4], 
-			[3.4,['a',1],'string'],
-			[3.4,'string',['a',1]], 
-			['string',['a',1],3.4],
-			['string',3.4,['a',1]]), results)
+			(['a',1],'string',3.4), 
+			(3.4,['a',1],'string'),
+			(3.4,'string',['a',1]), 
+			('string',['a',1],3.4),
+			('string',3.4,['a',1])), results)
