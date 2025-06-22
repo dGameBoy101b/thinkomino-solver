659f0d54b5e8b966ec063c597d7b40dc7ba810e1
dGameBoy101b <rhysmader@gmail.com>
2025-06-22 19:32:44 +0800

    Changed list rotator to iterator class to fit new tests

--- list_rotator.py
+++ list_rotator.py
@@ -1,29 +1,42 @@
-from typing import Iterator
+from typing import Iterable, Iterator
 
-def rotate_list(ls:list, step:int=1, iterations:int=None)->Iterator[list]:
-	#empty special case
-	if (len(ls) < 1):
-		return
-	#process step parameter
-	try:
-		step = int(step)
-	except ValueError as x:
-		raise TypeError('step must be an integer', x)
-	if step < 1:
-		raise ValueError('step must be 1 or greater')
-	if step > len(ls):
-		raise ValueError(f'step must be lesser than or equal to the length of the list ({len(ls)})')
-	#process iterations parameter
-	if iterations is None:
-		iterations = len(ls)
-	try:
-		iterations = int(iterations)
-	except ValueError as x:
-		raise TypeError('iterations must be an integer', x)
-	if iterations < 0:
-		raise ValueError('iterations must be 0 or greater')
-	#generate rotations
-	for i in range(iterations):
-		yield ls
-		ls = ls[-step:] + ls[:-step]
-	return
+def rotate_list(ls:list, step:int=1)->Iterator[tuple]:
+	return Rotations(ls, step)
+
+class Rotations(Iterator[Iterable]):
+	def __init__(self, iterable:Iterable, step:int=1):
+		self.iterable = tuple(iterable)
+		self.__step_index = 0
+		self.__len = None
+		try:
+			step = int(step)
+		except ValueError as x:
+			raise TypeError('step must be an integer', x)
+		if step < 1:
+			raise ValueError('step must be 1 or greater')
+		if len(self.iterable) > 0 and step > len(self.iterable):
+			raise ValueError(f'step must be lesser than or equal to the length of the iterable ({len(self.iterable)})')
+		self.step = step
+
+	def __len__(self) -> int:
+		if len(self.iterable) < 1:
+			return 1
+		if self.__len is None:
+			loops = 1
+			while len(self.iterable) * loops % self.step > 0:
+				loops += 1
+			self.__len = len(self.iterable) * loops // self.step
+		return self.__len
+
+	def __iter__(self):
+		return self
+	
+	def __next__(self) -> tuple:
+		if (self.__step_index >= len(self)):
+			raise StopIteration
+		if len(self.iterable) < 1:
+			self.__step_index += 1
+			return tuple()
+		index = -self.__step_index * self.step % len(self.iterable)
+		self.__step_index += 1
+		return self.iterable[index:] + self.iterable[:index]
