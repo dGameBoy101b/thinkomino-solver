53e471b192b04ffc68bdac068b01342388c8c13b
dGameBoy101b <rhysmader@gmail.com>
2025-06-22 21:02:01 +0800

    Changed permutation generator method to an iterator class to fit new tests

--- list_permuter.py
+++ list_permuter.py
@@ -1,11 +1,41 @@
 from typing import Iterator
+from math import factorial
 
-def permute_list(ls:list)->Iterator[list]:
-	#empty/single special case
-	if len(ls) < 2:
-		yield ls
-		return
-	#generate permutations
-	for index in range(len(ls)):
-		for permute in permute_list(ls[1:]):
-			yield permute[:index] + [ls[0]] + permute[index:]
+'''Obsolete: Use Permutations iterator class instead.'''
+def permute_list(ls:list)->Iterator[tuple]:
+	return Permutations(ls)
+
+class Permutations(Iterator[tuple]):
+	def __init__(self, iterable:tuple):
+		self.iterable = tuple(iterable)
+		self.__subpermuter = None if len(self.iterable) < 1 else Permutations(self.iterable[1:])
+		self.__subpermutation = None
+		self.__index = 0
+
+	def __len__(self)->int:
+		return factorial(len(self.iterable))
+
+	def __iter__(self):
+		return self
+	
+	def __next__(self)->tuple:
+		if self.__subpermuter is None:
+			if self.__index > 0:
+				raise StopIteration
+			self.__index = 1
+			return tuple()
+		
+		if self.__index >= len(self.iterable):
+			self.__subpermutation = None
+
+		if self.__subpermutation is None:
+			try:
+				self.__subpermutation = next(self.__subpermuter)
+			except StopIteration:
+				self.__subpermuter = None
+				raise StopIteration
+			self.__index = 0
+
+		permutation = self.__subpermutation[:self.__index] + (self.iterable[0],) + self.__subpermutation[self.__index:]
+		self.__index += 1
+		return permutation
