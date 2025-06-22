b63579d2b75f0c1aa2270b1cfdd8169defdcfc48
dGameBoy101b <rhysmader@gmail.com>
2025-06-22 19:32:02 +0800

    Changed list rotator tests. Added len assertions. Removed iterations cap. Changed list assertions to sequence assertions.

--- tests/list_rotator_test.py
+++ tests/list_rotator_test.py
@@ -4,27 +4,33 @@ from list_rotator import rotate_list
 class ListRotatorTests(TestCase):
 	
 	def test_empty(self):
+		rotator = rotate_list([])
+		self.assertEqual(len(rotator), 1)
+		self.assertSequenceEqual(next(rotator), [])
 		with self.assertRaises(StopIteration):
-			next(rotate_list([]))
+			next(rotator)
 	
-	def test_one(self):
+	def test_1(self):
 		rotator = rotate_list([1])
-		self.assertEqual(next(rotator), [1])
+		self.assertEqual(len(rotator), 1)
+		self.assertSequenceEqual(next(rotator), [1])
 		with self.assertRaises(StopIteration):
 			next(rotator)
 
-	def test_two(self):
+	def test_2(self):
 		rotator = rotate_list(['a',3.4])
-		self.assertEqual(next(rotator), ['a',3.4])
-		self.assertEqual(next(rotator), [3.4,'a'])
+		self.assertEqual(len(rotator), 2)
+		self.assertSequenceEqual(next(rotator), ['a',3.4])
+		self.assertSequenceEqual(next(rotator), [3.4,'a'])
 		with self.assertRaises(StopIteration):
 			next(rotator)
 
-	def test_three(self):
+	def test_3(self):
 		rotator = rotate_list([['a',1],3.4,'string'])
-		self.assertEqual(next(rotator),[['a',1],3.4,'string'])
-		self.assertEqual(next(rotator), ['string',['a',1],3.4])
-		self.assertEqual(next(rotator), [3.4,'string',['a',1]])
+		self.assertEqual(len(rotator), 3)
+		self.assertSequenceEqual(next(rotator),[['a',1],3.4,'string'])
+		self.assertSequenceEqual(next(rotator), ['string',['a',1],3.4])
+		self.assertSequenceEqual(next(rotator), [3.4,'string',['a',1]])
 		with self.assertRaises(StopIteration):
 			next(rotator)
 
@@ -40,40 +46,21 @@ class ListRotatorTests(TestCase):
 		with self.assertRaises(ValueError):
 			next(rotate_list([1,2,3], 4))
 
-	def test_step(self):
+	def test_5_step_2(self):
 		rotator = rotate_list([1,2,3,4,5], 2)
-		self.assertEqual(next(rotator), [1,2,3,4,5])
-		self.assertEqual(next(rotator), [4,5,1,2,3])
-		self.assertEqual(next(rotator), [2,3,4,5,1])
-		self.assertEqual(next(rotator), [5,1,2,3,4])
-		self.assertEqual(next(rotator), [3,4,5,1,2])
+		self.assertEqual(len(rotator), 5)
+		self.assertSequenceEqual(next(rotator), [1,2,3,4,5])
+		self.assertSequenceEqual(next(rotator), [4,5,1,2,3])
+		self.assertSequenceEqual(next(rotator), [2,3,4,5,1])
+		self.assertSequenceEqual(next(rotator), [5,1,2,3,4])
+		self.assertSequenceEqual(next(rotator), [3,4,5,1,2])
 		with self.assertRaises(StopIteration):
 			next(rotator)
 
-	def test_iteration_type_error(self):
-		with self.assertRaises(TypeError):
-			next(rotate_list([1,2,3], 1, 'string'))
-
-	def test_iteration_value_error(self):
-		with self.assertRaises(ValueError):
-			next(rotate_list([1,2,3], 1, -1))
-
-	def test_iteration_zero(self):
-		with self.assertRaises(StopIteration):
-			next(rotate_list([1,2,3], 1, 0))
-
-	def test_iteration_excess(self):
-		rotator = rotate_list([1,2,3], 1, 4)
-		self.assertEqual(next(rotator), [1,2,3])
-		self.assertEqual(next(rotator), [3,1,2])
-		self.assertEqual(next(rotator), [2,3,1])
-		self.assertEqual(next(rotator), [1,2,3])
-		with self.assertRaises(StopIteration):
-			next(rotator)
-
-	def test_iteration_trim(self):
-		rotator = rotate_list([1,2,3], 1, 2)
-		self.assertEqual(next(rotator), [1,2,3])
-		self.assertEqual(next(rotator), [3,1,2])
+	def test_4_step_2(self):
+		rotator = rotate_list([1,2,3,4], 2)
+		self.assertEqual(len(rotator), 2)
+		self.assertSequenceEqual(next(rotator), [1,2,3,4])
+		self.assertSequenceEqual(next(rotator), [3,4,1,2])
 		with self.assertRaises(StopIteration):
 			next(rotator)
