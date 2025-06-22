575b0916629afa8e401106d614eff4e6c36c16ac
dGameBoy101b <rhysmader@gmail.com>
2025-06-22 17:20:35 +0800

    Set default log config

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ from sys import stdin, stdout, stderr
 TILES_CSV_ARG_NAME = 'tiles_csv'
 SOLVER_PROCESSES_ARG_NAME = 'solver_processes'
 LOGGER_JSON_FILE_ARG_NAME = 'logger_config_file'
+DEFAULT_LOG_CONFIG_PATH = 'logger.json'
 SNAPSHOT_FOLDER_ARG_NAME = 'snapshot_folder'
 
 def arg_parser() -> ArgumentParser:
 	parser = ArgumentParser(description='Solve a Thinkomino puzzle')
 	parser.add_argument(TILES_CSV_ARG_NAME, type=str, help='Path to the csv file storing the available tiles')
 	parser.add_argument(SOLVER_PROCESSES_ARG_NAME, nargs='?', type=int, default=None, help='The maximum number of processes that can be used to solve generated boards')
-	parser.add_argument(LOGGER_JSON_FILE_ARG_NAME, nargs='?', type=str, default=None, help='Path to the json file used to configure a logger')
+	parser.add_argument(LOGGER_JSON_FILE_ARG_NAME, nargs='?', type=str, default=DEFAULT_LOG_CONFIG_PATH, help='Path to the json file used to configure a logger')
 	parser.add_argument(SNAPSHOT_FOLDER_ARG_NAME, nargs='?', type=str, default=None, help='Path to the folder used to export solution snapshots to')
 	return parser
 
