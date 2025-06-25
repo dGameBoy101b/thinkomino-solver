2bac2744c59cd4641ff8ce62cea339b0e69115a1
dGameBoy101b <rhysmader@gmail.com>
2025-06-25 18:40:41 +0800

    Converted board generator function to iterator class. Added len method to board generator.

--- thinkomino_board_generator.py
+++ thinkomino_board_generator.py
@@ -1,40 +1,36 @@
 from typing import Iterator
 from thinkomino_board import ThinkominoBoard
 from thinkomino_tile import ThinkominoTile
-from list_permuter import permute_list, Permutations
-from list_rotator import Rotations
-
-def permute_rotations(tiles: list[ThinkominoTile]) -> list[ThinkominoTile]:
-	if len(tiles) < 2:
-		yield tiles
-		return
-	for rotation in Rotations(tiles[0]):
-		for permutation in permute_rotations(tiles[1:]):
-			yield [ThinkominoTile(*rotation)] + permutation
+from list_permuter import Permutations
+from rotation_combiner import Rotateable, RotationCombinations, max_rotation_combinations
 
 def generate_boards(tiles: list[ThinkominoTile]) -> Iterator[ThinkominoBoard]:
-	for ordered_tiles in permute_list(tiles):
-		for state in permute_rotations(ordered_tiles):
-			yield ThinkominoBoard(*state)
+	yield from BoardGenerator(tiles)
 
 class BoardGenerator(Iterator[ThinkominoBoard]):
 	def __init__(self, tiles:tuple[ThinkominoTile]):
 		self.__orderer = Permutations(tiles)
-		self.__ordered_tiles = None
-		self.__rotators = list()
+		self.__rotation_combiner = None
 
 	def __iter__(self):
 		return self
 	
 	def __next__(self)->ThinkominoBoard:
-		if self.__ordered_tiles is None:
+		result = None
+		while result is None:
+			if self.__rotation_combiner is None:
+				try:
+					ordered_tiles = next(self.__orderer)
+				except StopIteration:
+					self.__rotation_combiner = None
+					self.__orderer = None
+					raise StopIteration
+				self.__rotation_combiner = RotationCombinations((Rotateable(tile) for tile in ordered_tiles))
 			try:
-				self.__ordered_tiles = next(self.__orderer)
+				result = next(self.__rotation_combiner)
 			except StopIteration:
-				self.__rotators = None
-				self.__orderer = None
-				raise StopIteration
-			self.__rotators = (Rotations(tile) for tile in self.__ordered_tiles)
-			
+				self.__rotation_combiner = None
+		return ThinkominoBoard(*(ThinkominoTile(*tile) for tile in result))
 
 	def __len__(self)->int:
+		return len(self.__orderer) * max_rotation_combinations(zip(self.__orderer.iterable, (1,)*len(self.__orderer.iterable)))
